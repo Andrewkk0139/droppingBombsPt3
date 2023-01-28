f0ad310e969a64e3574f9495d8565765db44121e
Andrewkk0139 <andrewkaiser2006@gmail.com>
2023-01-27 20:01:59 -0600

    finished step 43

---
+++ AK-droppingBombsPt2/Assets/TriggerExplosion.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerExplosion : MonoBehaviour
+{
+    [Header("Explosion parts")]
+    public GameObject explosion;
+
+    private void OnCollosionEnter(Collision collision)
+    {
+        Instantiate(explosion, transform.position, transform.rotation);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
